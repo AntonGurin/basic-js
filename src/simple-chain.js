335909357ea2a4934ab8e58223c4b07ef728492b
AntonGurin <79761911+AntonGurin@users.noreply.github.com>
2021-03-30 23:59:12 +0300

    Update: Code!

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,28 @@
-const CustomError = require("../extensions/custom-error");
-
 const chainMaker = {
+  arrLink: [],
+  
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+        return this.arrLink.length;
+    },
+    addLink(value) {
+        this.arrLink.push(`( ${value} )`);
+        return this;
+    },
+    removeLink(position) {
+        if ((position ^ 0) != position || position > this.arrLink.length || position < 1) {
+            this.arrLink=[]; throw new Error('wrong position');}
+        this.arrLink.splice(position-1, 1);
+        return this;
+    },
+    reverseChain() {
+        this.arrLink.reverse();
+        return this;
+    },
+    finishChain() {
+ let fin=this.arrLink.join('~~');
+ this.arrLink=[];
+ return fin;
+    }
 };
 
 module.exports = chainMaker;
