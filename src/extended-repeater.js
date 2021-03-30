cb08e73082b4dd23c4c1297a1bdb738672a30d89
AntonGurin <79761911+AntonGurin@users.noreply.github.com>
2021-03-30 23:48:38 +0300

    Change: code!

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,21 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    let result = '';
+    let reTi = options.repeatTimes;
+    let sep = options.separator!=undefined?options.separator:'+';
+    let addTemp = options.addition!==undefined?options.addition:'';
+    let add = '';
+    let addRT = options.additionRepeatTimes!=undefined?options.additionRepeatTimes:1;
+    let addSep = options.additionSeparator!=undefined?options.additionSeparator:'|';
+  
+    for( let j = 0; j < addRT; j++){
+      add += j == 0?addTemp:addSep+addTemp;
+    };
+    if(reTi==undefined){
+      return result += str+add;
+    };
+    for (let i=0; i<reTi; i++){
+      result += i==0?str+add:sep+str+add;
+    };
+    return result;
 };
   
